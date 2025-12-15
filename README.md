811a40138026d8e2cc6cdc63fd62f1d83b1d3489
Ahmad Yar <ahmadyar228@gmail.com>
2025-12-15 16:39:54 +0500

    updated readme and created at column

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Blog post
 
+- APIs to create, update & delete user
+- APIs to create, update & delete blog posts
+
 ## Local Setup
 
 - This project setup is done with `uv` from [astral docs](https://docs.astral.sh/uv/getting-started/installation/)
 - Install required packages using:
     `uv sync`
-- Create a `.env` file based on `example.env`, make sure to add right credentials
 - Run the script using:
     `uv run main.py`
--- models/models.py
+++ models/models.py
@@ -11,7 +11,7 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     username = Column(String, unique=True, index=True, nullable=False)
     full_name = Column(String)
-    created_at = Column(DateTime, default=datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.now(datetime.timezone.utc))
     
     posts = relationship("Post", back_populates="owner", cascade="all, delete-orphan")
 
@@ -23,7 +23,7 @@ class Post(Base):
     title = Column(String, nullable=False)
     content = Column(Text, nullable=False)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.now(datetime.timezone.utc))
+    updated_at = Column(DateTime, default=datetime.now(datetime.timezone.utc), onupdate=datetime.now(datetime.timezone.utc))
     
     owner = relationship("User", back_populates="posts")
